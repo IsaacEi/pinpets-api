541d0a44b630f37ec118ebb58dfc4cfd26a0be94
IsaacEi <12915389+IsaacEi@users.noreply.github.com>
2024-08-21 22:04:07 -0600

    Metodo para crear carpeta

--- src/controllers/image.controller.ts
+++ src/controllers/image.controller.ts
@@ -9,7 +9,7 @@ export async function fileUpload(req: Request, res: Response): Promise<any> {
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).json({
             estatus: false,
-            msg: 'No hay ningún archivo'
+            mensaje: 'No hay ningún archivo'
         });
     }
     // Procesar la imagen...
@@ -28,6 +28,9 @@ export async function fileUpload(req: Request, res: Response): Promise<any> {
     }
     // Path para guardar la imagen
     const pathImg = path.join( __dirname, `../public/${ folder }/${imagen}`);
+    const pathFolder = path.join( __dirname, `../public/${ folder }`);
+    // Crear la carpeta si no existe
+    await createFolder(pathFolder);
     // Mover la imagen
     file.mv( pathImg , (err: any) => {
         if (err){
@@ -82,6 +85,34 @@ export async function download(req: Request, res: Response): Promise<any> {
         return res.download( pathImgDefecto );
     }
 
+}
+
+
+// Metodo para crear carpeta
+async function createFolder(folderPath: string) : Promise<any> {
+    return new Promise( async (resolve) => {
+        try {
+
+            if (!fs.existsSync(folderPath)) {
+                fs.mkdirSync(folderPath)
+                return resolve({ 
+                    estatus: true,
+                    mensaje: 'Folder creado correctamente'
+                });
+            }
+
+            return resolve({ 
+                estatus: true,
+                mensaje: 'Folder exite'
+            });
+            
+        } catch (err) {
+            return resolve({ 
+                estatus: false,
+                mensaje: '!Error¡ folder no creado'
+            });
+        }
+    });
 }
 
 
